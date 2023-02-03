f823afa9113a5be5e412b6aef1389d419cab3b4b
Michael <michael2ga3@gmail.com>
2023-02-04 01:45:14 +0300

    Split many params user

--- fakegeo/user.py
+++ fakegeo/user.py
@@ -1,95 +1,100 @@
-from telethon import TelegramClient
 import sqlite3
+
+from telethon import TelegramClient
+from type import Api
+from type import UserInfo
 
 
 class User:
-    _session_name: str
-    _username: str
-    _chat_id: int
-    _phone: str
-    _auth_code: int
-    _api_id: str
-    _api_hash: str
+    _api: Api
+    _info: UserInfo
     _client: TelegramClient
     _path_db: str
 
     def __init__(self,
-                 api_id: str,
-                 api_hash: str,
-                 session_name: str,
-                 username: str,
-                 chat_id: int,
-                 phone: str,
-                 auth_code: int,
+                 api: Api,
+                 info: UserInfo,
                  client: TelegramClient,
                  path_db: str = 'user.db'):
-        self._api_id = api_id
-        self._api_hash = api_hash
-        self._session_name = session_name
-        self._username = username
-        self._chat_id = chat_id
-        self._phone = phone
-        self._auth_code = auth_code
+        self._api = api
+        self._info = info
         self._client = client
         self._path_db = path_db
 
     def __getattr__(self, name: str):
-        return self.__dict__[f'{name}']
+        return self.__dict__[name]
 
     def __setattr__(self, name: str, value):
-        self.__dict__[f'{name}'] = value
+        self.__dict__[name] = value
 
     def save(self):
         conn = sqlite3.connect(self._path_db)
         c = conn.cursor()
-        c.execute('''
+        api = self._api
+        info = self._info
+        c.execute("""
             CREATE TABLE IF NOT EXISTS users (
-                session_name text,
-                username text,
-                chat_id integer,
-                phone text,
-                auth_code integer,
-                api_id text,
-                api_hash text
+                api_id TEXT,
+                api_hash TEXT,
+                session_name TEXT,
+                username TEXT,
+                chat_id INTEGER,
+                phone TEXT,
+                auth_code INTEGER,
+                schedule TEXT
+                ):
             )
-        ''')
-        c.execute('''
-            INSERT INTO users (session_name,
- username,
- chat_id,
- phone,
- auth_code,
- api_id,
- api_hash)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''',
-                  (self._session_name,
-                   self._username,
-                   self._chat_id,
-                   self._phone,
-                   self._auth_code,
-                   self._api_id,
-                   self._api_hash))
+        """)
+        c.execute("""
+            INSERT INTO users (
+                api_id,
+                api_hash,
+                session_name,
+                username,
+                chat_id,
+                phone,
+                auth_code,
+                schedule
+ )
+            VALUES (?,?,?,?,?,?,?)
+        """, (
+            api._api_id,
+            api._api_hash,
+            info._session_name,
+            info._username,
+            info._chat_id,
+            info._phone,
+            info._auth_code,
+            info._schedule
+        ))
         conn.commit()
         conn.close()
 
     @classmethod
-    def load(cls, session_name: str, path_db: str):
+    def load(cls, chat_id: str, path_db: str):
         conn = sqlite3.connect(path_db)
         c = conn.cursor()
-        c.execute('''
-            SELECT * FROM users WHERE session_name=?
-        ''', (session_name,))
-        result = c.fetchone()
-        if result:
-            session_name, username, chat_id, phone, code, id, hash = result
-            client = TelegramClient(session_name, id, hash)
-            return cls(session_name,
-                       username,
-                       chat_id,
-                       phone,
-                       code,
-                       id,
-                       hash,
-                       client)
-        return None
+        c.execute("SELECT * FROM users WHERE chat_id=?", (chat_id))
+        row = c.fetchone()
+        conn.close()
+
+        (api_id,
+         api_hash,
+         session_name,
+         username,
+         chat_id,
+         phone,
+         auth_code,
+         schedule) = row
+        client = TelegramClient(
+            session=session_name, api_id=api_id, api_hash=api_hash)
+        user_info = UserInfo(
+            session_name,
+            username,
+            int(chat_id),
+            phone,
+            auth_code,
+            schedule
+        )
+        api = Api(api_id, api_hash)
+        return cls(api, user_info, client, path_db)
