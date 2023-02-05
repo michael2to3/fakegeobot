aff6039262d45e6d2620856f886b131e08f1ab3c
Michael <michael2ga3@gmail.com>
2023-02-05 10:22:45 +0300

    Fix query, Add specific path for db, connection now open and close in one-one method

--- fakegeo/session.py
+++ fakegeo/session.py
@@ -1,6 +1,6 @@
 import logging
 import sqlite3
-from sqlite3 import Connection, Cursor
+from sqlite3 import Connection
 from typing import Iterable
 
 from telethon import TelegramClient
@@ -11,29 +11,23 @@ from user import User
 
 class Session:
     logger: logging.Logger
+    _name_db: str
     _path_db: str
-    _connect: Connection
-    _cursor: Cursor
 
-    def __init__(self, path_db: str):
+    def __init__(self, path_db: str, name_db: str):
         self.logger = logging.getLogger(__name__)
-        self._path_db = path_db
+        self._name_db = name_db
+        self._path_db = path_db + '/' + self._name_db
 
-    def connection(self):
-        self._connect = sqlite3.connect(self._path_db)
-        return self
+        self.create_table()
 
-    def close(self):
-        self._connect.close()
-        return self
+    def connect(self) -> Connection:
+        return sqlite3.connect(self._path_db)
 
-    def cursor(self):
-        self._cursor = self._connect.cursor()
-        return self
-
-    def createTable(self):
+    def create_table(self):
         self.logger.debug('Create table')
-        self._cursor.execute('''
+        con = self.connect()
+        con.cursor().execute('''
             CREATE TABLE IF NOT EXISTS users (
                 api_id TEXT,
                 api_hash TEXT,
@@ -46,10 +40,8 @@ class Session:
                 active BOOLEAN
                 )
         ''')
-        return self
-
-    def commit(self):
-        self._connect.commit()
+        con.commit()
+        con.close()
         return self
 
     def _insert(self, user: User):
@@ -57,7 +49,8 @@ class Session:
         api = user._api
         info = user._info
         active = user._active
-        self._cursor.execute('''
+        con = self.connect()
+        con.cursor().execute('''
             INSERT INTO users (
                 api_id,
                 api_hash,
@@ -69,31 +62,34 @@ class Session:
                 schedule,
                 active
  )
-            VALUES (?,?,?,?,?,?,?,?)
+            VALUES (?,?,?,?,?,?,?,?,?)
         ''', (
             api._api_id,
             api._api_hash,
             info._session_name,
             info._username,
-            info._chat_id,
+            str(info._chat_id),
             info._phone,
             info._auth_code,
             info._schedule,
-            active
+            str(int(active))
         ))
+        con.commit()
+        con.close()
+        return self
 
     def _update(self, user: User):
         self.logger.debug('Update user data', str(user))
         api = user._api
         info = user._info
         active = user._active
-        self._cursor.execute('''
+        con = self.connect()
+        con.cursor().execute('''
             UPDATE users SET
                 api_id=?,
                 api_hash=?,
                 session_name=?,
                 username=?,
-                chat_id=?,
                 phone=?,
                 auth_code=?,
                 schedule=?,
@@ -107,42 +103,63 @@ class Session:
             info._phone,
             info._auth_code,
             info._schedule,
-            info._chat_id,
-            active
+            str(int(active)),
+            str(info._chat_id)
         ))
+        con.commit()
+        con.close()
 
     def save(self, user: User):
         chat_id = user._info._chat_id
-        current = self.load(chat_id)
 
-        if current is None:
-            self._insert(user)
-        else:
+        if self.check_exists(chat_id):
             self._update(user)
+        else:
+            self._insert(user)
 
         return self
 
     def delete(self, chat_id: int):
         self.logger.debug('Remove user ', str(chat_id))
         id = str(chat_id)
-        self._cursor.execute('DELETE FROM users WHERE chat_id=?', (id))
+        sql = 'DELETE FROM users WHERE chat_id=?'
+        con = self.connect()
+        con.cursor().execute(sql, (id))
+        con.commit()
+        con.close()
 
     def loadAll(self) -> Iterable[User]:
         self.logger.debug('Load add users')
-        self._cursor.execute('SELECT * FROM users')
-        rows = self._cursor.fetchall()
+        con = self.connect()
+        cursor = con.cursor()
+        cursor.execute('SELECT * FROM users')
+        rows = cursor.fetchall()
+        con.close()
         for row in rows:
             yield self._generate(row)
 
     def load(self, chat_id: int) -> User:
         self.logger.debug('Load user with chat_id ', str(chat_id))
         id = str(chat_id)
-        self._cursor.execute('SELECT * FROM users WHERE chat_id=?', (id))
-        row = self._cursor.fetchone()
+        con = self.connect()
+        cursor = con.cursor()
+        cursor.execute('SELECT * FROM users WHERE chat_id=?', (id,))
+        row = cursor.fetchone()
+        con.close()
 
         return self._generate(row)
 
+    def check_exists(self, chat_id: int) -> bool:
+        try:
+            self.load(chat_id)
+        except TypeError:
+            return False
+        return True
+
     def _generate(self, row) -> User:
+        if row is None:
+            raise TypeError('Not found, row is None')
+
         (api_id,
          api_hash,
          session_name,
@@ -152,10 +169,12 @@ class Session:
          auth_code,
          schedule,
          active) = row
+
         client = TelegramClient(
             session=session_name,
             api_id=api_id,
             api_hash=api_hash)
+
         user_info = UserInfo(
             session_name,
             username,
@@ -165,4 +184,4 @@ class Session:
             schedule
         )
         api = Api(api_id, api_hash)
-        return User(api, user_info, client, active, self._path_db)
+        return User(api, user_info, client, active)
