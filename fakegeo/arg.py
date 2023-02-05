7cb5a5a21e4f3581773113a6dcfa9fa5e9972f71
Michael <michael2ga3@gmail.com>
2023-02-05 10:15:17 +0300

    Rewrite Arg - Now phone number u can get for raw message or specific format - Code get now work for both method get message from bot - Remove static method - Add logger

--- fakegeo/arg.py
+++ fakegeo/arg.py
@@ -1,25 +1,49 @@
+import logging
+
+
 class Arg:
-    @staticmethod
-    def get_phone(text: str) -> str:
-        parts = text.split()
-        if parts is None or len(parts) != 2:
-            raise ValueError('Not valid parse string')
+    logger: logging.Logger
 
-        phone = parts[1]
-        if phone is None or len(phone.strip()) == 0:
-            raise ValueError('Not valid phone number')
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
 
-        return phone
+    def get_phone(self, text: str) -> str:
+        self.logger.debug('Get phone from string', text)
 
-    @staticmethod
-    def get_auth_code(text: str) -> int:
-        parts = text.split()
-        if parts is None or len(parts) > 1:
-            raise ValueError('Not valid message')
+        def remove_junk(utext: str) -> str:
+            junk = [' ', '(', ')', '_']
+            for i in junk:
+                utext = utext.replace(i, '')
+            return utext
 
-        code = parts[0]
-        if code is None or len(code.strip()) == 0:
-            raise ValueError('Not valid auth code')
+        phone = remove_junk(text)
+
+        index_first_digit = [i.isdigit() for i in phone].index(True)
+        has_plus = phone[index_first_digit - 1] == '+'
+
+        phone = phone[index_first_digit:].rstrip()
+
+        has_not_allow_symbol = any(not i.isdigit() for i in phone)
+        if has_not_allow_symbol:
+            raise ValueError('Phone number has not allow symbol')
+
+        if len(phone) < 10 or len(phone) > 13:
+            raise ValueError('Phone format is not valid')
+
+        preffix = '+' if has_plus else ''
+        return preffix + phone
+
+    def get_auth_code(self, text: str) -> int:
+        self.logger.debug('Get auth code from string', text)
+
+        code = text
+        index_first_digit = [i.isdigit() for i in code].index(True)
+
+        code = code[index_first_digit:].rstrip()
+
+        has_not_allow_symbol = any(not i.isdigit() for i in code)
+        if has_not_allow_symbol:
+            raise ValueError('Auth code has not allow symbol')
 
         tg_len_code = 5
         if len(code) != tg_len_code:
