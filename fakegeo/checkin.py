689b248fc42653c6f2141b2c2f1a39425d2db33d
Michael <michael2ga3@gmail.com>
2023-02-08 19:00:43 +0300

    Add connect if send location

--- fakegeo/checkin.py
+++ fakegeo/checkin.py
@@ -1,7 +1,7 @@
 import logging
 
 from aiocron import crontab
-from telethon import TelegramClient
+from telethon.sync import TelegramClient
 from telethon.types import InputMediaGeoLive
 
 from geolocation import Geolocation
@@ -15,19 +15,27 @@ class CheckIn:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    async def send_live_location(self, user: TelegramClient) -> None:
+    async def send_live_location(self, user: User) -> None:
+
+        client = user.instance_telegramclient()
+        phone_number = user._info._phone
+        auth_code = user._info._auth_code
+
+        await client.connect()
+        client.start(phone_number, code_callback=lambda: auth_code)
+
         geo = Geolocation()
         # Well... Ignore error from library tg, it's ok
         # Still send InputMediaGeoLive
         stream: InputMediaGeoLive = geo.get()
-        await user.send_message(
+        await client.send_message(
             self._to_username,
             file=stream)
 
     def run(self, user: User):
         cron: str = user._info._schedule
-        tg = user.instance_telegramclient()
-        def func(): return self.send_live_location(tg)
+        client = user.instance_telegramclient()
+        def func(): return self.send_live_location(client)
         return crontab(cron, func=func, start=True)
 
     def pass_cron(self):
