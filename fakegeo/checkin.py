92f4e7eaa1f95a356ecef4e215707b9f062591ea
Michael <michael2ga3@gmail.com>
2023-02-10 06:44:31 +0300

    Split method to cron action

--- fakegeo/checkin.py
+++ fakegeo/checkin.py
@@ -1,10 +1,7 @@
 import logging
 
 from aiocron import crontab
-from telethon.types import InputMediaGeoLive
-from proxytelegram import ProxyTelegram
-
-from geolocation import Geolocation
+from cronaction import CronAction
 from user import User
 
 
@@ -15,32 +12,21 @@ class CheckIn:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    async def send_live_location(self, user: User) -> None:
-
-        phone_number = user._info._phone
-        auth_code = user._info._auth_code
-        hash = user._info._phone_code_hash
-
-        client = ProxyTelegram.get_client(user)
-        await client.connect()
-        await client.sign_in(phone_number, auth_code, phone_code_hash=hash)
-
-        geo = Geolocation()
-        # Well... Ignore error from library tg, it's ok
-        # Still send InputMediaGeoLive
-        stream: InputMediaGeoLive = geo.get()
-        await client.send_message(
-            self._to_username,
-            file=stream)
+    async def send_live_location(self, user: User):
+        name = self._to_username
+        return await CronAction.send_live_location(user, name)
 
     def run(self, user: User):
-        cron: str = user._info._schedule
-        def func(): return self.send_live_location(user)
-        return crontab(cron, func=func, start=True)
+        cron = user._info._schedule
+        name = self._to_username
+        id = user._info._chat_id
+
+        async def _cron_action():
+            self.logger.debug(f'Trigger cron from {id} to {name}')
+            await CronAction.send_live_location(user, name)
+
+        return crontab(cron, func=_cron_action, start=True)
 
     def pass_cron(self):
         schedule = '30 18 * * 6'
-
-        def func():
-            pass
-        return crontab(schedule, func=func, start=False)
+        return crontab(schedule, start=False)
