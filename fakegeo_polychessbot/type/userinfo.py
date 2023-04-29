32324f008561a81eb886264ebcc9d98c250cb7cf
Michael <michael2ga3@gmail.com>
2023-04-29 10:15:03 +0300

    refactoring struct type userinfo

--- fakegeo_polychessbot/type/userinfo.py
+++ fakegeo_polychessbot/type/userinfo.py
@@ -1,12 +1,4 @@
 class UserInfo:
-    _session_name: str
-    _username: str
-    _chat_id: int
-    _phone: str
-    _auth_code: int
-    _schedule: str
-    _phone_code_hash: str
-
     def __init__(
         self,
         session_name: str,
@@ -17,16 +9,66 @@ class UserInfo:
         schedule: str,
         phone_code_hash: str,
     ):
-        self._session_name = session_name
-        self._username = username
-        self._chat_id = chat_id
-        self._phone = phone
-        self._auth_code = auth_code
-        self._schedule = schedule
-        self._phone_code_hash = phone_code_hash
+        self.session_name = session_name
+        self.username = username
+        self.chat_id = chat_id
+        self.phone = phone
+        self.auth_code = auth_code
+        self.schedule = schedule
+        self.phone_code_hash = phone_code_hash
 
-    def __getattr__(self, name: str):
-        return self.__dict__[name]
+    @property
+    def session_name(self) -> str:
+        return self._session_name
 
-    def __setattr__(self, name: str, value):
-        self.__dict__[name] = value
+    @session_name.setter
+    def session_name(self, value: str):
+        self._session_name = value
+
+    @property
+    def username(self) -> str:
+        return self._username
+
+    @username.setter
+    def username(self, value: str):
+        self._username = value
+
+    @property
+    def chat_id(self) -> int:
+        return self._chat_id
+
+    @chat_id.setter
+    def chat_id(self, value: int):
+        self._chat_id = value
+
+    @property
+    def phone(self) -> str:
+        return self._phone
+
+    @phone.setter
+    def phone(self, value: str):
+        self._phone = value
+
+    @property
+    def auth_code(self) -> int:
+        return self._auth_code
+
+    @auth_code.setter
+    def auth_code(self, value: int):
+        self._auth_code = value
+
+    @property
+    def schedule(self) -> str:
+        return self._schedule
+
+    @schedule.setter
+    def schedule(self, value: str):
+        self._schedule = value
+
+    @property
+    def phone_code_hash(self) -> str:
+        return self._phone_code_hash
+
+    @phone_code_hash.setter
+    def phone_code_hash(self, value: str):
+        self._phone_code_hash = value
