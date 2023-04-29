d9f9d38e390f987bff5b6a9a95a11779c8588155
Michael <michael2ga3@gmail.com>
2023-04-29 23:05:04 +0300

    uptodate code use fakelocation execute in send command

--- fakegeo_polychessbot/_commands/send.py
+++ fakegeo_polychessbot/_commands/send.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from telethon.errors import AuthKeyUnregisteredError
 from _commands import Command
 from _type import Session, User
+from _action import Fakelocation
 
 
 class Send(Command):
@@ -9,14 +11,10 @@ class Send(Command):
         chat_id = update.message.chat_id
         emess = "Well done"
         try:
-            await self.bot._users.perform_checkin(chat_id)
-        except cronaction.FloodError as e:
-            emess = f"Flood detection! Wait {e.timeout}"
+            action = Fakelocation(self.bot._api, self.bot._users[chat_id])
+            await action.execute()
         except AuthKeyUnregisteredError as e:
-            self.logger.error(str(e))
+            self.bot.logger.error(str(e))
             emess = "Your token is not registered"
-        except Exception as e:
-            self.logger.error(str(e))
-            emess = "Oops something went wrong"
         finally:
             await update.message.reply_text(emess)
