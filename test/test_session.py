612313fec69ed623332b81573d83c15f6ae06fc3
Michael <michael2ga3@gmail.com>
2023-04-29 16:58:06 +0300

    update test session

--- test/test_session.py
+++ test/test_session.py
@@ -80,4 +80,4 @@ class SessionTest(unittest.TestCase):
             session.save(us._info._chat_id)
 
         loads = list(session._session.load_all())
-        self.assertGreater(len(loads), 10)
+        self.assertEqual(len(loads), 10)
