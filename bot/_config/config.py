87a05cccd14db75746ad4fc6509dd35be8c64c5a
Michael <michael2ga3@gmail.com>
2023-04-30 13:51:00 +0300

    api id and api hash inside in api app in config

--- bot/_config/config.py
+++ bot/_config/config.py
@@ -1,12 +1,14 @@
 from decouple import config
+from model import ApiApp
 
 
 class Config:
-    __slots__ = ("_api_id", "_api_hash", "_bot_token", "_db_path", "_db_name")
+    __slots__ = ("_api", "_bot_token", "_db_path", "_db_name")
 
     def __init__(self):
-        self._api_id = int(self._get_config("API_ID"))
-        self._api_hash = self._get_config("API_HASH")
+        self._api = ApiApp(
+            int(self._get_config("API_ID")), self._get_config("API_HASH")
+        )
         self._bot_token = self._get_config("BOT_TOKEN")
         self._db_path = self._get_config("SQLITE_PATH")
         self._db_name = self._get_config("SQLITE_NAME")
@@ -18,12 +20,8 @@ class Config:
         raise Exception("Get variable from config is not found - " + name)
 
     @property
-    def api_id(self):
-        return self._api_id
-
-    @property
-    def api_hash(self):
-        return self._api_hash
+    def api(self):
+        return self._api
 
     @property
     def bot_token(self):
