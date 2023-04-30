ba1c10b185750fb88d41a43246de8589d44cd607
Michael <michael2ga3@gmail.com>
2023-04-30 20:51:49 +0300

    refactoring config

--- bot/_config/config.py
+++ bot/_config/config.py
@@ -1,36 +1,25 @@
+from dataclasses import dataclass
 from decouple import config
 from model import ApiApp
 
 
+@dataclass
 class Config:
-    __slots__ = ("_api", "_bot_token", "_db_path", "_db_name")
+    api: ApiApp
+    bot_token: str
+    db_path: str
+    db_name: str
+    cron_timeout: int
 
-    def __init__(self):
-        self._api = ApiApp(
-            int(self._get_config("API_ID")), self._get_config("API_HASH")
-        )
-        self._bot_token = self._get_config("BOT_TOKEN")
-        self._db_path = self._get_config("SQLITE_PATH")
-        self._db_name = self._get_config("SQLITE_NAME")
+    def __post_init__(self):
+        self.api = ApiApp(int(self._get_config("API_ID")), self._get_config("API_HASH"))
+        self.bot_token = self._get_config("BOT_TOKEN")
+        self.db_path = self._get_config("SQLITE_PATH")
+        self.db_name = self._get_config("SQLITE_NAME")
+        self.cron_timeout = int(self._get_config("CRON_TIMEOUT"))
 
     def _get_config(self, name: str) -> str:
         output = config(name)
         if isinstance(output, str):
             return output
         raise Exception("Get variable from config is not found - " + name)
-
-    @property
-    def api(self):
-        return self._api
-
-    @property
-    def bot_token(self):
-        return self._bot_token
-
-    @property
-    def db_path(self):
-        return self._db_path
-
-    @property
-    def db_name(self):
-        return self._db_name
