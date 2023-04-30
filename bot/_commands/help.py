4a94e2e3e5bef54f95e6fb7a9049c008f5e07301
Michael <michael2ga3@gmail.com>
2023-04-30 08:48:37 +0300

    update tab in reply text in commands

--- bot/_commands/help.py
+++ bot/_commands/help.py
@@ -7,20 +7,20 @@ class Help(Command):
     async def handle(self, update: Update, _: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             """
-    /start - Start the bot 🚀
-    /help - Show this help message 📚
-    /auth PHONE_NUMBER - Authenticate with your phone number 📱
-        Example: /auth +79992132533
-    /code CODE - Enter the received code 🔢
-        Example: /code 2.8.2.0.4
-    /schedule CRON - Set a message sending schedule with a cron expression ⏰
-        Example: /schedule 30 18 * * 5
-    /send - Send your fake geolocation now 🌐
-        Example: /send
-    /delete - Delete your token and all related data 🗑️
-        Example: /delete
-    Cron help website: https://cron.help/#30_18_*_*_5
-    More info: https://github.com/michael2to3/fakegeo-polychessbot
-    Support: https://t.me/+EGnT6v3APokxMGYy
+/start - Start the bot 🚀
+/help - Show this help message 📚
+/auth PHONE_NUMBER - Authenticate with your phone number 📱
+    Example: /auth +79992132533
+/code CODE - Enter the received code 🔢
+    Example: /code 2.8.2.0.4
+/schedule CRON - Set a message sending schedule with a cron expression ⏰
+    Example: /schedule 30 18 * * 5
+/send - Send your fake geolocation now 🌐
+    Example: /send
+/delete - Delete your token and all related data 🗑️
+    Example: /delete
+Cron help website: https://cron.help/#30_18_*_*_5
+More info: https://github.com/michael2to3/fakegeo-polychessbot
+Support: https://t.me/+EGnT6v3APokxMGYy
     """
         )
