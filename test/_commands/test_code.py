86c1bd9980ae435dc9393b634c5864ad00c469db
Michael <michael2ga3@gmail.com>
2023-05-02 01:44:00 +0300

    update tests

--- test/_commands/test_code.py
+++ test/_commands/test_code.py
@@ -18,7 +18,19 @@ class TestCode(asynctest.TestCase):
         self.api = MagicMock(spec=ApiApp)
         self.token = "fake_token"
         self.db = MagicMock(spec=DatabaseHandler)
-        self.bot = Bot(self.api, self.token, self.db)
+        self.bot = MagicMock(spec=Bot)
+        self.bot.users = self.create_mock_users()
+
+    def create_mock_users(self):
+        users = {}
+        for i in range(1, 6):
+            user = MagicMock(spec=User)
+            user.session.chat_id = i
+            user.cron = MagicMock(spec=Cron)
+            user.cron.expression = "*/5 * * * *"
+            user.cron.timeout = 300
+            users[i] = user
+        return users
 
     async def test_handle(self):
         try:
@@ -39,15 +51,11 @@ class TestCode(asynctest.TestCase):
             update.message.text = "/code 1.2.3.4.5"
             update.message.reply_text = AsyncMock()
 
-            self.bot.users[1] = MagicMock(spec=User)
-            mock_cron = MagicMock(spec=Cron)
-            mock_cron.expression = "*/5 * * * *"
-            mock_cron.timeout = 300
-            self.bot.users[1].cron = mock_cron
             self.bot.db.save_user = MagicMock()
 
             await code_command.handle(update, context)
 
+            code_command._get_cron(self.bot.users[1]).stop()
             update.message.reply_text.assert_called()
         except CroniterBadCronError as e:
             self.fail(f"CroniterBadCronError raised: {e}")
