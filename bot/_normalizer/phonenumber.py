7802d29360ff6dc27fbf795367de12f13ebd0d17
Michael <michael2ga3@gmail.com>
2023-05-01 08:37:17 +0300

    refactoring phonenumber

--- bot/_normalizer/phonenumber.py
+++ bot/_normalizer/phonenumber.py
@@ -1,25 +1,18 @@
 from .normalizer import Normalizer
+import re
 
 
 class PhoneNumber(Normalizer):
     @staticmethod
     def normalize(text: str) -> str:
-        phone = PhoneNumber._remove_junk(text)
-
-        index_first_digit = [i.isdigit() for i in phone].index(True)
-        has_plus = phone[index_first_digit - 1] == "+"
-
-        phone = phone[index_first_digit:].rstrip()
-
-        has_not_allow_symbol = any(not i.isdigit() for i in phone)
-        if has_not_allow_symbol:
-            raise ValueError("Phone number has not allow symbol")
+        phone = PhoneNumber._remove_junk(text.strip())
+        has_plus = phone.startswith("+")
+        phone = re.sub(r"\D", "", phone)
 
         if len(phone) < 10 or len(phone) > 13:
             raise ValueError("Phone format is not valid")
 
-        preffix = "+" if has_plus else ""
-        return preffix + phone
+        return f"+{phone}"
 
     @staticmethod
     def _remove_junk(utext: str) -> str:
