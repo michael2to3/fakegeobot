e7f43af82d2fb9e55f454a6c4cffb52f75a2cdee
Michael <michael2ga3@gmail.com>
2023-05-07 07:59:53 +0300

    add timeout in connect client

--- bot/_user/requestcode.py
+++ bot/_user/requestcode.py
@@ -9,7 +9,7 @@ class RequestCode:
         if phone is None:
             raise ValueError("Please enter your phone number")
 
-        client = TelegramClient(user.session.session_name, api.id, api.hash)
+        client = TelegramClient(user.session.session_name, api.id, api.hash, timeout=120)
         await client.connect()
 
         req = await client.send_code_request(phone)
